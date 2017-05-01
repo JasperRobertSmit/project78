11d4ecfd67f0a21637bb1e6d83e5021fb8d1b618
MathijsL <mathijs__lagendijk@msn.com>
2017-05-01 14:58:02 +0200

    GestureManager

--- Assets/GestureManager.cs
+++ Assets/GestureManager.cs
@@ -5,6 +5,7 @@ using UnityEngine.VR.WSA.Input;
 
 public class GestureManager : MonoBehaviour
 {
+    public static GameObject gameObject;
 
     // Use this for initialization
     void Start()
@@ -17,23 +18,41 @@ public class GestureManager : MonoBehaviour
         recognizer.TappedEvent += (source, tapCount, ray) =>
         {
             Add();
-            Debug.Log("Tap");
         };
         recognizer.StartCapturingGestures();
     }
 
     public void Add()
     {
-        Object itemPrefab = Resources.Load("Assets/Resources/Cube");
+        Debug.Log("add");
 
-        GameObject itemObject = (GameObject)Instantiate(itemPrefab);
-        itemObject.transform.parent = Camera.allCameras[0].gameObject.transform;
-        itemObject.transform.position = Camera.allCameras[0].gameObject.transform.position;
+        if (gameObject == null)
+        {
+            Debug.Log("add1");
+            Object itemPrefab = Resources.Load("Cube");
+            gameObject = (GameObject)Instantiate(itemPrefab, transform.position + (transform.forward * 5), transform.rotation);
+        }
+        else
+        {
+            Debug.Log("add2");
+            gameObject = null;
+        }
+        
+        //GameObject itemObject = (GameObject)Instantiate(itemPrefab);
+        //itemObject.transform.parent = Camera.allCameras[0].gameObject.transform;
+        //itemObject.transform.position = Camera.allCameras[0].gameObject.transform.position;
+
+        //Debug.Log(itemObject.transform.position.x + " " + itemObject.transform.position.y + " " + itemObject.transform.position.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        Debug.Log("update1");
+        if (gameObject != null)
+        {
+            Debug.Log("update2");
+            gameObject.transform.position = transform.position + (transform.forward * 5);
+        }
     }
 }
